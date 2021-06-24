05eb40fbe78e83ac3a41008c83a167486c4766d0
Elton <eolorteguiarce@gmail.com>
2021-06-24 01:19:56 -0500

    Pantalla login, pagina principal, pantalla pedidos(Input buscar y cardlista)

---
+++ app/src/main/java/com/ixcorp/rapitel_app/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.ixcorp.rapitel_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button btnIniciarSesion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        asignarReferencias();
+    }
+
+    private void asignarReferencias(){
+
+        btnIniciarSesion = findViewById(R.id.btnIniciarSesion);
+
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
